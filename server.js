83506446ae6a56eb771593744d63db3cf0c0b0e6
Galitan <galitan.dev@gmail.com>
2020-11-14 14:20:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const moment = require('moment');
 const generatePdf = require('./generatePDF');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(async (req, res) => {
 
